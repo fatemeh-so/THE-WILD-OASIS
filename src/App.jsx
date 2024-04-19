10b3f1f81fe8fb2b7e17a54701def0b726b3451e
Fatemeh Soleimani <fwtemeh.soleimanii@gmail.com>
2024-04-19 13:05:41 -0800

    -

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,8 @@ import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 
 import GlobalStyles from "./styles/GlobalStyles";
 import Dashboard from "./pages/Dashboard";
-import Bookings from "./pages/Bookings";
 import Booking from "./pages/Booking";
+import Bookings from "./pages/Bookings";
 import Cabins from "./pages/Cabins";
 import Users from "./pages/Users";
 import Settings from "./pages/Settings";
@@ -16,7 +16,7 @@ import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { Toaster } from "react-hot-toast";
 import CheckIn from "./pages/CheckIn";
 import ProtectedRoute from "./ui/ProtectedRoute";
-import {DarkModeProvider} from "./contexts/DarkModeContext";
+import { DarkModeProvider } from "./contexts/DarkModeContext";
 const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
